1f105026571c25572d051ac60535a69d233e1230
Abbas Abdulla <abbass.abdulla.naser@gmail.com>
2024-12-06 18:34:22 +0300

    updated section group btn

--- src/components/SectionGroupButtons/SectionGroupButtons.jsx
+++ src/components/SectionGroupButtons/SectionGroupButtons.jsx
@@ -10,25 +10,27 @@ export default function SectionGroupButtons({
 }) {
   return (
     <div className="section-group-btn-outer-container">
-      <div className="section-group-btns">
-        <h2 className="section-title">{title}</h2>
-        {btns.map((btn, index) => {
-          return (
-            <button
-              key={index}
-              className={
-                componentToShowState === btn ? selectedBtnClassName : ""
-              }
-              onClick={() => {
-                const element = document.getElementById(sectionId);
-                element.scrollIntoView({ behavior: "smooth" });
-                setComponentToShowState(btn);
-              }}
-            >
-              {btn}
-            </button>
-          );
-        })}
+      <div className="section-group_inner-container">
+        <div className="section-group-btns">
+          <h2 className="section-title">{title}</h2>
+          {btns.map((btn, index) => {
+            return (
+              <button
+                key={index}
+                className={
+                  componentToShowState === btn ? selectedBtnClassName : ""
+                }
+                onClick={() => {
+                  const element = document.getElementById(sectionId);
+                  element.scrollIntoView({ behavior: "smooth" });
+                  setComponentToShowState(btn);
+                }}
+              >
+                {btn}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
